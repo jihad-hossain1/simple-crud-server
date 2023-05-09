77dc198719f3f4c7355fa90a927d55326a82c5ab
jihad-hossain1 <jihadkhan934@gmail.com>
2023-05-09 19:42:45 +0600

    last change update

--- index.js
+++ index.js
@@ -46,7 +46,7 @@ async function run() {
 
     app.post('/users', async (req, res) => {
       const user = req.body;
-      console.log('new user', user);
+      // console.log('new user', user);
       const result = await userCollection.insertOne(user);
       res.send(result)
 
